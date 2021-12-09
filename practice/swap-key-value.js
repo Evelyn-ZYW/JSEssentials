47f2c04675ecdb716ed4acb9597bdcba8f165e13
Evelyn Z <evelynzdragon@gmail.com>
2021-12-08 16:42:40 -0800

    completed the coding problem from previous interview

--- practice/swap-key-value.js
+++ practice/swap-key-value.js
@@ -1,7 +1,10 @@
 // SWAP KEY VALUE
 let oldObj = { a: "1", b: "2" };
 let newObj = {}; // should become { 1: 'a', 2: 'b' }
+
 Object.keys(oldObj).forEach((key) => {
   newObj[oldObj[key]] = key;
+  console.log(oldObj[key])
 });
+
 console.log(newObj);
