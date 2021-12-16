e1dd0c1290ccc0f69005054706b837a41592444c
Evelyn Z <evelynzdragon@gmail.com>
2021-12-16 11:47:33 -0800

    methods in class; methods chaining

--- object-oriented-js/class.js
+++ object-oriented-js/class.js
@@ -3,22 +3,39 @@ class User {
   constructor(username, email) {
     this.username = username;
     this.email = email;
+    this.score = 0;
   }
   login() {
     console.log(`${this.username} just logged in.`);
+    return this
   }
   logout() {
     console.log(`${this.username} just logged out.`);
+    return this
+  }
+  incScore(){
+    this.score += 1;
+    console.log(`${this.username} has a score of ${this.score}`)
+    return this
+  }
+}
+
+class Admin extends User {
+  deleteUser(user) {
+    users = users.filter((u) => {
+      return u.username !== user.username;
+    });
   }
 }
 const userOne = new User("evelyn", "evelyn@email.com");
+const userTwo = new User("frank", "frank@email.com");
+const userThree = new Admin("gougou", "gougou@email.com");
 
-/* create the User function */
-// function User(username, email) {
-//   this.username = username;
-//   this.email = email;
-// }
-// const userOne = new User("evelyn", "evelyn@email.com");
+let users = [userOne, userTwo, userThree];
 
-console.log(userOne);
-userOne.logout();
+// console.log(userOne, userTwo, userThree);
+// userOne.logout();
+userOne.login().incScore().logout()
+
+userThree.deleteUser(userOne);
+console.log(users);
