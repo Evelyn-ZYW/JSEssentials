b193127d7e3f35b638bb1347ca97510817eafc04
Evelyn Z <evelynzdragon@gmail.com>
2021-12-21 20:06:41 -0800

    new problem

---
+++ practice/string-concat.js
@@ -0,0 +1,26 @@
+// Write a function that takes two strings, say a and b, as arguments
+// If a contains b, append b to the beginning of a
+// If not, append it to the end
+// Return the concatenation
+
+function myFunction() {
+
+    return
+ }
+
+
+console.log(myFunction('cheese', 'cake'))
+// Expected
+// 'cheesecake'
+
+console.log(myFunction('lips', 's'))
+// Expected
+// 'slips'
+
+console.log(myFunction('Java', 'script'))
+// Expected
+// 'Javascript'
+
+console.log(myFunction(' think, therefore I am', 'I'))
+// Expected
+// 'I think, therefore I am'
