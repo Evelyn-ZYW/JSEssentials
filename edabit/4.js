c4cf99c0fa2aa8bb9cff4818d0ced5212297419c
Evelyn Z <evelynzdragon@gmail.com>
2021-12-24 13:33:06 -0800

    edabit practice 4

---
+++ edabit/4.js
@@ -0,0 +1,17 @@
+// Take an array of integers (positive or negative or both) and return the sum of the absolute value of each element.
+
+function getAbsSum(arr) {
+  return arr.map((num) => Math.abs(num)).reduce((a, b) => a + b);
+}
+
+console.log(getAbsSum([2, -1, 4, 8, 10]));
+//  ➞ 25
+
+console.log(getAbsSum([-3, -4, -10, -2, -3]));
+//  ➞ 22
+
+console.log(getAbsSum([2, 4, 6, 8, 10]));
+//  ➞ 30
+
+console.log(getAbsSum([-1]));
+//  ➞ 1
