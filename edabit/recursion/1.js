1a8d701620d7daf1efd8cf5219710ac6e4f033df
Evelyn Z <evelynzdragon@gmail.com>
2022-01-12 17:27:40 -0800

    simple example of recursion

--- edabit/recursion/1.js
+++ edabit/recursion/1.js
@@ -1,17 +1,8 @@
-// Create a function which takes a parameter n and returns a function such that it, when called n times, returns the string "edabit".
-
-function lambdaDepth(n) {
-  return n === 0 ? "edabit" : () => lambdaDepth(n - 1);
+function fac(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * fac(n - 1);
 }
 
-console.log(lambdaDepth(0));
-//  ➞ "edabit"
-
-console.log(lambdaDepth(1)());
-//  ➞ "edabit"
-
-console.log(lambdaDepth(2)()());
-//  ➞ "edabit"
-
-console.log(typeof lambdaDepth(2)());
-//  ➞ "function"
+console.log(fac(5));
